99f363e7a6b77599a9dab9de93efe9904ec17a05
Jack Case <jackacase@gmail.com>
2022-04-01 21:51:15 -0400

    Keyboard driver (#4)
    Need to debug unknown IOCTL kernel messages, but it does what was intended. 
    
    * initial commit of matrix_keypad driver
    
    Copied from kernel source
    needs to be modified to poll while any key is held down
    this will hopefully address issue #3
    
    * updated .gitignore for module compile artifacts
    
    * updated .gitignore for module compile artifacts
    
    * added makefile and fixed module_build.sh
    
    * updated comment header and module documentation macros
    
    * Added initial code to support polling of the keypad
    
    When a key is pressed triggering an interrupt, polling will be enabled.
    When scanning the keypad, if no keys are held, polling will be disabled.
    
    Untested.
    
    * missing ;
    
    * added convenience all and clean targets to keyboard driver makefile

--- software/container/module_build.sh
+++ software/container/module_build.sh
@@ -1,2 +1,4 @@
+#! /bin/bash
+
 cd module_src
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -C /home/root/kernel M=`pwd`
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -C /home/root/kernel M=`pwd` modules
